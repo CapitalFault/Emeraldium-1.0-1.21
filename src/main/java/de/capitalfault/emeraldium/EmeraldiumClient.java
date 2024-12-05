dc92e9adfdef9e6c0beb95f94af74b7867f27182
CapitalFault <brueschke.felix@web.de>
2024-12-06 00:57:59 +0100

    First Commit

---
+++ src/main/java/de/capitalfault/emeraldium/EmeraldiumClient.java
@@ -0,0 +1,10 @@
+package de.capitalfault.emeraldium;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EmeraldiumClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
