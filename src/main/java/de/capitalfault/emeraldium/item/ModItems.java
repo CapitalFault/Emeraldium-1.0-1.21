0089c61d5fe2738f4e8ea301d6bcf6b8f92bc167
CapitalFault <brueschke.felix@web.de>
2024-12-07 00:53:10 +0100

    Added a emerald sword + a few more important things

--- src/main/java/de/capitalfault/emeraldium/item/ModItems.java
+++ src/main/java/de/capitalfault/emeraldium/item/ModItems.java
@@ -4,6 +4,8 @@ import de.capitalfault.emeraldium.Emeraldium;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
+import net.minecraft.item.SwordItem;
+import net.minecraft.item.ToolMaterials;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -12,6 +14,9 @@ import net.minecraft.util.Identifier;
 public class ModItems {
     public static final Item PINK_GARNET = registerItem("pink_garnet", new Item(new Item.Settings()));
     public static final Item RAW_PINK_GARNET = registerItem("raw_pink_garnet", new Item(new Item.Settings()));
+    public static final Item EMERALD_SWORD = registerItem("emerald_sword",
+            new SwordItem(ModToolMaterials.EMERALD, new Item.Settings()
+                    .attributeModifiers(SwordItem.createAttributeModifiers(ModToolMaterials.EMERALD, 3, -2.4f))));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, Identifier.of(Emeraldium.MOD_ID, name), item);
@@ -24,6 +29,10 @@ public class ModItems {
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
             entries.add(PINK_GARNET);
             entries.add(RAW_PINK_GARNET);
+        });
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.COMBAT).register(entries -> {
+            entries.add(EMERALD_SWORD);
         });
     }
 }
---
+++ src/main/java/de/capitalfault/emeraldium/util/ModTags.java
@@ -0,0 +1,31 @@
+package de.capitalfault.emeraldium.util;
+
+import de.capitalfault.emeraldium.Emeraldium;
+import net.minecraft.item.Item;
+import net.minecraft.block.Block;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.tag.TagKey;
+import net.minecraft.util.Identifier;
+
+//import static sun.security.util.DerValue.createTag;
+
+public class ModTags {
+    public static class Blocks {
+        public static final TagKey<Block> NEEDS_EMERALD_TOOL = createTag("needs_emerald_tool");
+        public static final TagKey<Block> INCORRECT_FOR_EMERALD_TOOL = createTag("incorrect_for_emerald_tool");
+
+        public static TagKey<Block> createTag(String name) {
+            return TagKey.of(RegistryKeys.BLOCK, Identifier.of(Emeraldium.MOD_ID, name));
+        }
+
+
+    }
+
+    public static class Items {
+
+
+        public static TagKey<Item> createTag(String name) {
+            return TagKey.of(RegistryKeys.ITEM, Identifier.of(Emeraldium.MOD_ID, name));
+        }
+    }
+}
